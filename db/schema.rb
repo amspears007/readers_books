a9b423dda3302d8736de89b8b2e537e3c40ee48c
Amy Spears <amspears007@gmail.com>
2023-05-10 11:58:20 -0600

    Add reader index view page and index page route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_232918) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_172505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_232918) do
     t.bigint "reader_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
     t.index ["reader_id"], name: "index_books_on_reader_id"
   end
 

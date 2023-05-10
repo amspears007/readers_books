b43d4bcfc268b80a98155e87ba2eeeddbb2504e3
Amy Spears <amspears007@gmail.com>
2023-05-10 15:24:44 -0600

    Add formatting in view page for lists and within block and additional tests

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -10,21 +10,40 @@ require 'rails_helper'
     let!(:gone) {amy.books.create!(title: "Then She was Gone", author:"Lisa Jewell", genre: "thriller", year_written: 2017, fiction: true)}
     let!(:deep_work) {amy.books.create!(title: "Deep Work", author:"Cal Newport", genre: "self help", year_written: 2016, fiction: false)}
     let!(:metal) {todd.books.create!(title: "The Lost Metal", author:"Brandon Sanderson", genre: "fantasy", year_written: 2022, fiction: true)}
-    let!(:penguins) {penny.books.create!(title: "Mr Popper's Penguins", author: "Richard & Florence Atwater", genre:"Children's literature", fiction: true)}
+    let!(:penguins) {penny.books.create!(title: "Mr Popper's Penguins", author: "Richard & Florence Atwater", genre:"Children's literature", year_written: 1938, fiction: true)}
 
     describe "US3 As a visitor When I visit '/child_table_name' " do
       it " I see each book in the system including the book's attributes" do
         visit "/books"
         save_and_open_page
 
+        within("h1") do
+          expect(page).to have_content("Books Index Page")
+        end
 
-          within("#book-info-#{plum_creek.id}") do
-        expect(page).to have_content(plum_creek.title)
-        expect(page).to have_content(plum_creek.author)
-        expect(page).to have_content(plum_creek.genre)
-        expect(page).to have_content(plum_creek.year_written)
-        expect(page).to have_content(plum_creek.fiction)
-          end
+        within("#book-info-#{plum_creek.id}") do
+          expect(page).to have_content("Title: #{plum_creek.title}")
+          expect(page).to have_content("Author: #{plum_creek.author}")
+          expect(page).to have_content("Genre: #{plum_creek.genre}")
+          expect(page).to have_content("Year Written: #{plum_creek.year_written}")
+          expect(page).to have_content("Fiction: #{plum_creek.fiction}")
+        end
+
+        within("#book-info-#{gone.id}") do
+          expect(page).to have_content("Title: #{gone.title}")
+          expect(page).to have_content("Author: #{gone.author}")
+          expect(page).to have_content("Genre: #{gone.genre}")
+          expect(page).to have_content("Year Written: #{gone.year_written}")
+          expect(page).to have_content("Fiction: #{gone.fiction}")
+        end
+
+        within("#book-info-#{penguins.id}") do
+          expect(page).to have_content("Title: #{penguins.title}")
+          expect(page).to have_content("Author: #{penguins.author}")
+          expect(page).to have_content("Genre: #{penguins.genre}")
+          expect(page).to have_content("Year Written: #{penguins.year_written}")
+          expect(page).to have_content("Fiction: #{penguins.fiction}")
         end
       end
     end
+  end

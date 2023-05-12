8163c5844c9f555b37d13aba3dcc5c0fa56587ea
Amy Spears <amspears007@gmail.com>
2023-05-12 14:55:58 -0600

    Add new actions to controller and new view page

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,12 +1,16 @@
 class ReadersController < ApplicationController
   def index
-    @readers = Reader.all
-    @sorted_readers = Reader.sort_by_most_recently_created
+    # @readers = Reader.all
+    @readers = Reader.sort_by_most_recently_created
     
   end
 
   def show
     @reader = Reader.find(params[:id])
     @book_number= @reader.books.size
+  end
+
+  def new
+  
   end
 end

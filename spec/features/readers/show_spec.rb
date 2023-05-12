3a9c2f777c05827c6efefede11f76787e8a6e1d9
Amy Spears <amspears007@gmail.com>
2023-05-11 18:19:52 -0600

    Add tests for parent_child link

--- spec/features/readers/show_spec.rb
+++ spec/features/readers/show_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
   RSpec.describe 'Parent Show', type: :feature do
-    let!(:amy) {Reader.create!(name: "Amy Spears", age:40, avid_reader: false)}
-    let!(:todd) {Reader.create!(name: "Todd Van Wiel", age:45, avid_reader: true)}
-    let!(:penny) {Reader.create!(name: "Penny Van Wiel", age:8, avid_reader: false)}
+    let!(:amy) {Reader.create!(name: "Amy", age:40, avid_reader: false)}
+    let!(:todd) {Reader.create!(name: "Todd", age:45, avid_reader: true)}
+    let!(:penny) {Reader.create!(name: "Penny", age:8, avid_reader: false)}
 
     let!(:plum_creek) {amy.books.create!(title: "On the Banks of Plum Creek", author:"Laura Ingalls Wilder", genre: "children's historical novels", year_written: 1937, fiction: true)}
     let!(:gone) {amy.books.create!(title: "Then She was Gone", author:"Lisa Jewell", genre: "thriller", year_written: 2017, fiction: true)}
@@ -44,14 +44,25 @@ require 'rails_helper'
         expect(current_path).to eq("/readers")
       end
 
-    describe "US8 Parent Index Link" do
+    describe "US9 Parent Index Link" do
       it "I see a link at the top of the page that takes me to the Child Index" do
         visit "/readers/#{amy.id}"
 
         expect(page).to have_link("List All Books")
         click_link "List All Books"
         expect(current_path).to eq("/books")
+        end
       end
+
+
+    describe "US10 Parent Child Index Link" do
+      it "I see a link to take me to that parent's `child_table_name` page ('/parents/:id/child_table_name')" do
+        visit "/readers/#{amy.id}"
+
+        expect(page).to have_link("List of #{amy.name}'s Books")
+        click_link "List of #{amy.name}'s Books"
+        expect(current_path).to eq("/readers/#{amy.id}/books")
+        end
       end
     end
   end

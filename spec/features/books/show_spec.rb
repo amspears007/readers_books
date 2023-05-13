ba38740e5f60baa5fdf83817829225dce2926708
Amy Spears <amspears007@gmail.com>
2023-05-13 14:50:37 -0600

    Add first part of tests for US14 for update book edit link

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-  RSpec.describe 'Child Index', type: :feature do
+  RSpec.describe 'Child Show', type: :feature do
     let!(:amy) {Reader.create!(name: "Amy", age:40, avid_reader: false)}
     let!(:todd) {Reader.create!(name: "Todd", age:45, avid_reader: true)}
     let!(:penny) {Reader.create!(name: "Penny", age:8, avid_reader: false)}
@@ -44,6 +44,16 @@ require 'rails_helper'
         expect(page).to have_content("Genre: #{penguins.genre}")
         expect(page).to have_content("Year Published: #{penguins.year_written}")
         expect(page).to have_content("Fiction Classification: #{penguins.fiction}")
+        end
+      end
+
+      describe "US14 Child Update" do
+        it "I see a link to update that Child 'Update Book' when I click the link I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes.  When I click the button to submit the form 'Update Book" do 
+          visit "books/#{metal.id}"
+
+          expect(page).to have_link("Update Book")
+          click_link("Update Book")
+          expect(current_path).to eq("/books/#{metal.id}/edit")
         end
       end
     end

ffe88afb710eb21fa5097f0e5f150c55f65eda30
Amy Spears <amspears007@gmail.com>
2023-05-13 15:30:19 -0600

    Add update method to controller and patch to routes and form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,16 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    # require 'pry'; binding.pry
+    book.update(title: params[:title], author: params[:author], genre: params[:genre], year_written: params[:year_written], fiction: params[:fiction])
+    redirect_to "/books/#{book.id}"
+
+  end
 end
